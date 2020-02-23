7c7e3f55571aff248de482b8bc8b53f55fe1abb4
Ghada6336 <ghadafahmie6336@gmail.com>
2020-02-23 14:55:18 +0200

    task1

--- flights/views.py
+++ flights/views.py
@@ -1,1 +1,14 @@
 from .models import Flight, Booking
+from django.shortcuts import render
+from datetime import datetime 
+from rest_framework.generics import ListAPIView
+from .serializers import FlightSerializer, BookingSerializer
+
+class FlightList(ListAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class BookingList(ListAPIView):
+    queryset = Booking.objects.filter(date__gt=datetime.today())
+    serializer_class = BookingSerializer
--- task_1/urls.py
+++ task_1/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from flights import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('bookings/', views.BookingList.as_view(), name="bookings-list"),
+    path('flights/',views.FlightList.as_view(), name="flights-list"),
+
 ]
